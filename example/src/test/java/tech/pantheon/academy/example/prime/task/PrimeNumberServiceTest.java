61a11ac98651dc5e48a481e215ae3ecb8e3cc961
ivan.martiniak <ivan.martiniak@pantheon.tech>
2021-08-04 10:20:06 +0200

    Added negative test for prime number checker.
    Signed-off-by: ivan.martiniak <ivan.martiniak@pantheon.tech>
    Change-Id: Ie7e624307f74065c868521885047fc2edf12ab92

--- example/src/test/java/tech/pantheon/academy/example/prime/task/PrimeNumberServiceTest.java
+++ example/src/test/java/tech/pantheon/academy/example/prime/task/PrimeNumberServiceTest.java
@@ -1,10 +1,11 @@
 package tech.pantheon.academy.example.prime.task;
 
 import com.google.common.collect.ImmutableSet;
-import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tech.pantheon.academy.example.prime.solution.PrimeNumberServiceImpl;
+
+import java.util.Set;
 
 class PrimeNumberServiceTest {
 
@@ -21,11 +22,29 @@ class PrimeNumberServiceTest {
             139,
             61);
 
+    private static final Set<Integer> notPrimes = ImmutableSet.of(
+            167218993,
+            16383533,
+            578593,
+            135673,
+            329173,
+            798233,
+            1393,
+            615);
+
     @Test
     void isPrime() {
         final PrimeNumberService primeNumberService = new PrimeNumberServiceImpl();
         for (final Integer prime : primes) {
             Assertions.assertTrue(primeNumberService.isPrime(prime), "Prime check failed, tested prime: " + prime);
+        }
+    }
+
+    @Test
+    void isNotPrime() {
+        final PrimeNumberService primeNumberService = new PrimeNumberServiceImpl();
+        for (final Integer number : notPrimes) {
+            Assertions.assertFalse(primeNumberService.isPrime(number), "Prime check failed, tested number: " + number);
         }
     }
 }
