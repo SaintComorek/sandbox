29ee2d8a09f64cc942ff4e134317c9b24ebb26be
Bacinsky, Vladimir <Vladimir.Bacinsky@pantheon.tech>
2021-12-21 11:56:45 +0100

    Update the content of the assignment
    Signed-off-by: Bacinsky, Vladimir <Vladimir.Bacinsky@pantheon.tech>
    Change-Id: I01b05f9cc97ebacd2c66098a02686647dabd3072

--- union-find/percolation/README.md
+++ union-find/percolation/README.md
@@ -1,18 +1,20 @@
-# Percolation 
+# Percolation
 More information on percolation theory [Click Me] https://en.wikipedia.org/wiki/Percolation_theory.
 
-## The model 
+## The model
 
-We model a percolation system using an n-by-n grid of sites. Each site is either open or blocked. A full site is an open site that can be connected to an open site in the top row via a chain of neighboring (left, right, up, down) open sites. We say the system percolates if there is a full site in the bottom row. In other words, a system percolates if we fill all open sites connected to the top row and that process fills some open site on the bottom row.
+We model a percolation system using an n-by-n grid of sites. Each site is either open or blocked. A full site is an open site that can be connected to an open site in the top row via a chain of neighboring (left, right, up, down) open sites. Open site in the top row is also full site. If open site is not full then it is empty open site. We say the system percolates if there is a full site in the bottom row. In other words, a system percolates if we fill all open sites connected to the top row and that process fills some open site on the bottom row.
 
 ### Model that does percolate
 Note full open site connected from bottom to top.
 ![percolates](./does_percolate.svg)
-### Model that does not percollate 
+### Model that does not percollate
 There is no open site connected from bottom to top.
 ![does not percolate](./does_not_percolate.svg)
+### Grid-1
+![grid-1](./grid-1.svg)
 
-## The task 
+## The task
 Write an implementation of Percolation. Tests will load grid from text files. Each test file will contain a grid n rows by n columns containing information on open sites indicated by value 1 and blocked sites indicated by value 0.
 
 ```java
@@ -21,17 +23,15 @@ public class Percolation {
     // loads grid from a file
     public void Percolation(File f)
 
-    // opens the site (row, col) if it is not open already
-    void open(int row, int col)
 
     // is the site (row, col) open?
-    boolean isOpen(int row, int col)
+    boolean isSiteOpen(int row, int col)
 
     // is the site (row, col) full?
-    boolean isFull(int row, int col)
+    boolean isSiteFull(int row, int col)
 
-    // returns the number of open sites
-    int openSitesCount()
+    // returns the number of disjunctive sets of open sites
+    int countSetsAfterUnifySets()
 
     // does the system percolate?
     boolean percolates()
--- union-find/percolation/src/test/java/sk/pantheon/academy/union/percolation/PercolationTest.java
+++ union-find/percolation/src/test/java/sk/pantheon/academy/union/percolation/PercolationTest.java
@@ -1,61 +1,112 @@
 package sk.pantheon.academy.union.percolation;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.File;
-import java.net.URISyntaxException;
-import org.junit.jupiter.api.Test;
 
 public class PercolationTest {
 
     @Test
-    public void grid1ShouldPercolate() throws URISyntaxException {
+    public void wrongNameFile()  {
+        Exception exception = assertThrows(FileNotFoundException.class, () -> {
+            new Percolation(new File(
+                    "src/test/resources/grid-4.txt"));
+        });
+        String expectedMessage = "Could not find file.";
+        Assertions.assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void grid1ShouldPercolate() throws Exception {
         File gridFile = getGridFile("grid-1.txt");
         final Percolation percolation = new Percolation(gridFile);
         assertTrue(percolation.percolates());
     }
 
     @Test
-    public void grid1ShouldHave3OpenSites() throws URISyntaxException {
+    public void grid1ShouldHave21OpenSites() throws Exception {
         File gridFile = getGridFile("grid-1.txt");
         final Percolation percolation = new Percolation(gridFile);
-        assertEquals(3, percolation.openSitesCount());
+        assertEquals(21, percolation.countSetsAfterUnifySets());
     }
 
     @Test
-    public void grid1SiteShouldBeFull() throws URISyntaxException {
+    public void siteWithIndexesR0C0Ingrid1ShouldBeFull() throws Exception {
         File gridFile = getGridFile("grid-1.txt");
         final Percolation percolation = new Percolation(gridFile);
-        assertTrue(percolation.isFull(0, 0));
-    }
-    @Test
-    public void grid2ShouldHave2OpenSites() throws URISyntaxException {
-        File gridFile = getGridFile("grid-2.txt");
-        final Percolation percolation = new Percolation(gridFile);
-        assertEquals(2, percolation.openSitesCount());
-    }
-    @Test
-    public void grid2SiteShouldBeFull() throws URISyntaxException {
-        File gridFile = getGridFile("grid-2.txt");
-        final Percolation percolation = new Percolation(gridFile);
-        assertTrue(percolation.isFull(0, 0));
-    }
-    @Test
-    public void grid3ShouldHave0OpenSites() throws URISyntaxException {
-        File gridFile = getGridFile("grid-3.txt");
-        final Percolation percolation = new Percolation(gridFile);
-        assertEquals(0, percolation.openSitesCount());
-    }
-    @Test
-    public void grid3SiteShouldNotBeFull() throws URISyntaxException {
-        File gridFile = getGridFile("grid-3.txt");
-        final Percolation percolation = new Percolation(gridFile);
-        assertFalse(percolation.isFull(0, 0));
+        assertTrue(percolation.isSiteFull(0, 0));
     }
 
+    @Test
+    public void siteWithIndexesR3C10Ingrid1ShouldBeFull() throws Exception {
+        File gridFile = getGridFile("grid-1.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertTrue(percolation.isSiteFull(3, 10));
+    }
+
+    @Test
+    public void siteWithIndexesR5C15Ingrid1ShouldBeNotFull() throws Exception {
+        File gridFile = getGridFile("grid-1.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertFalse(percolation.isSiteFull(5, 15));
+    }
+
+    @Test
+    public void grid2ShouldPercolate() throws Exception {
+        File gridFile = getGridFile("grid-2.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertTrue(percolation.percolates());
+    }
+
+    @Test
+    public void grid2ShouldHave23OpenSites() throws Exception {
+        File gridFile = getGridFile("grid-2.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertEquals(23, percolation.countSetsAfterUnifySets());
+    }
+
+    @Test
+    public void siteWithIndexesR0C0Ingrid2ShouldBeFull() throws Exception {
+        File gridFile = getGridFile("grid-2.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertTrue(percolation.isSiteFull(0, 0));
+    }
+
+    @Test
+    public void siteWithIndexesR7C2Ingrid2ShouldBeFull() throws Exception {
+        File gridFile = getGridFile("grid-2.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertTrue(percolation.isSiteFull(7, 2));
+    }
+
+    @Test
+    public void grid3ShouldHave27OpenSites() throws Exception {
+        File gridFile = getGridFile("grid-3.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertEquals(27, percolation.countSetsAfterUnifySets());
+    }
+
+    @Test
+    public void siteWithIndexesR0C0Ingrid3ShouldBeFull() throws Exception {
+        File gridFile = getGridFile("grid-3.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertTrue(percolation.isSiteFull(0, 0));
+    }
+
+    @Test
+    public void grid3ShouldNotPercolate() throws Exception{
+        File gridFile = getGridFile("grid-3.txt");
+        final Percolation percolation = new Percolation(gridFile);
+        assertFalse(percolation.percolates());
+    }
     private File getGridFile(final String fileName) throws URISyntaxException {
         return new File(this.getClass().getClassLoader().getResource(fileName).toURI());
     }
